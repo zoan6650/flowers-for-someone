beae14bfd8a14a9b033b5e429e51a2173272d520
zoan6650 <zoanmafia@gmail.com>
2025-06-20 22:47:59 +0930

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something`.split('')
+const text = `Happy Anniversary Adrian`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
